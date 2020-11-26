2ade451c83cec7717c3a20c8975198706bd2926e
Sumedh Raj <rsumedh0077@gmail.com>
2020-11-26 18:09:09 +0530

    Authentication Completed

--- App.tsx
+++ App.tsx
@@ -5,11 +5,12 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify'
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native'
 import config from './aws-exports'
 Amplify.configure(config)
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -24,3 +25,4 @@ export default function App() {
     );
   }
 }
+export default withAuthenticator(App);
