8b4e3cf0f41376ae3ccdeb7178ee8c0b7da7879c
Sumedh Raj <rsumedh0077@gmail.com>
2020-11-27 01:14:30 +0530

    Added New Users

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, FlatList } from 'react-native'
 import ChatListItem from '../components/ChatListItem';
 
-import users from '../data/users'
+//import users from '../data/users'
 import ContactListItem from '../components/ContactListItem';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../graphql/queries';
 
 export default function ContactScreen() {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const userData = await API.graphql(graphqlOperation(listUsers));
+
+                setUsers(userData.data.listUsers.items)
+            } catch (err) {
+
+            }
+        }
+        fetchUsers()
+    }, [])
     return (
         <View style={{ backgroundColor: 'white' }}>
             <FlatList
